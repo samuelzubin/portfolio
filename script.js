2248b5cd2cdaa1ef5bbd0e6de55d4590b5b72dc0
samuelzubin <szubin06@gmail.com>
2025-10-15 19:21:55 -0700

    Created navigation menu

--- script.js
+++ script.js
@@ -1,16 +1,37 @@
 const intro = document.querySelector('.about-intro');
+let clicked = false;
+
+function toggleMenu() {
+    const menu = document.getElementById("menu");
+    
+    if (!clicked) {
+        menu.style.visibility = "visible";
+        menu.classList.add('open');
+        clicked = true;
+    }
+    
+    else {
+        menu.classList.remove('open');
+        clicked = false;
+    }
+}
+
+function closeMenu() {
+    clicked = true;
+    toggleMenu();
+}
 
 function isOnScreen(div) {
     const rect = div.getBoundingClientRect();
     return rect.top < window.innerHeight && rect.bottom >= 0;
 }
 
-function checkScroll() {
+function beginTyping() {
     if (isOnScreen(intro)) {
         intro.classList.add('animate');
-        window.removeEventListener('scroll', checkScroll);
+        window.removeEventListener('scroll', beginTyping);
     }
 }
 
-window.addEventListener('scroll', checkScroll);
-checkScroll();
+window.addEventListener('scroll', beginTyping);
+beginTyping();
