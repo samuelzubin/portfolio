292692fbce45ca7057cb828092cb2746ab29044f
samuelzubin <szubin06@gmail.com>
2025-10-22 23:17:05 -0700

    Created quote fading animation

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 const intro = document.querySelector('.about-intro');
+const quote = document.querySelector('em')
+const quote_list = ["\"Tell me and I forget. Teach me and I may remember. Involve me and I learn.\"", 
+                    "\"When you change the way you look at things, the things you look at change.\"",
+                    "\"We are often so caught up with whether or not we could, that we don't stop to think if we should.\""
+                ]
+let i = 0;
+
 let clicked = false;
 
 function toggleMenu() {
@@ -21,6 +28,14 @@ function closeMenu() {
     toggleMenu();
 }
 
+function changeQuote() {
+    quote.textContent = quote_list[0]
+    setInterval(() => {
+        i = i + 1
+        quote.textContent = quote_list[i % 3]
+    }, 15000);
+}
+
 function isOnScreen(div) {
     const rect = div.getBoundingClientRect();
     return rect.top < window.innerHeight && rect.bottom >= 0;
@@ -32,6 +47,8 @@ function beginTyping() {
         window.removeEventListener('scroll', beginTyping);
     }
 }
+
+setTimeout(changeQuote, 18000)
 
 window.addEventListener('scroll', beginTyping);
 beginTyping();
